ca801315411512eb82ef02b5ad778a134a707c6b
fazioandrea.f <fazioandrea.f@gmail.com>
2021-04-23 17:57:19 +0200

    ed ex 9

---
+++ components/ProductDisplay.js
@@ -0,0 +1,105 @@
+app.component('product-display',{
+    props:{
+        premium:{
+            type:Boolean,
+            //prop validation
+            required:true,
+        },
+        detaaails:{
+            type:String,
+            required:true,
+        }
+    },
+    
+    
+    
+    template:
+    /*html*/
+    `        <div class="product-container">
+          <div class="product-image">
+            <img 
+            :class="!inStock?'out-of-stock-img':''"
+            :src="image" >
+          </div>
+          <div class="product-info">
+            <h1>{{title}}</h1>
+            <p v-if="inStock>10">In Stock</p>
+            <p v-else-if="inStock<=10 && inStock>0">Almost sold out</p>
+            <!-- <p v-if="inStock">In Stock</p> -->
+            <!-- lo stesso ma modifica la visibilità -->
+            <!-- <p v-show="inStock">In Stock</p> -->
+            <p v-else>Out of stock</p>
+            <p>Shipping: {{shipping}} {{detaaails}}</p>
+            <a :href="url">Clicca</a>
+            <p v-if="onSale">{{titleOnSale}}</p>
+            <ul>
+              <li v-for="detail in details">{{detail}}</li>
+            </ul>
+            <div 
+            v-for="(variant,index) in variants" 
+            key="variant.id" 
+            @mouseover="updateVariant(index)"
+            class="color-circle"
+            :style="{backgroundColor: variant.color}">
+            </div>
+            <!-- v-on == @ -->
+            <button class="button" v-on:click="addToCart"
+            :class="{disabledButton: !inStock}"
+            :disabled="!inStock">Add to Cart</button>
+            <button class="button" v-on:click="downToCart">Remove to Cart</button>
+          </div>
+        </div>`,
+            data(){
+        return{
+            product:'Socks',
+            description:'Lorem ipsum dolor',
+            // image:'./assets/images/socks_green.jpg',
+            selectedVariant:0,
+            url:'google.com',
+            // inStock:true,
+            // inventory:0,
+            onSale:true,
+            details:['50% cotton','30% wool','20% polyester'],
+            variants:[
+                {id:2234,color:'green', image:'./assets/images/socks_green.jpg',quantity:50},
+                {id:2235,color:'blue', image:'./assets/images/socks_blue.jpg',quantity:0}],
+            brand:'Vue mastery',
+        }
+    },
+    methods:{
+     addToCart(){
+         this.cart +=1;
+     },
+     updateVariant(index){
+         this.selectedVariant=index;
+     },
+     downToCart(){
+         if(this.cart>0)
+        this.cart -=1
+        
+     },
+    },
+    // lika calculatore, si aggiorna solo quando serve
+    computed:{
+        title(){
+            return this.brand +' '+this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        titleOnSale(){
+            if(this.onSale)
+            return this.brand +' '+this.product +' is on sale'
+        },
+        shipping(){
+            if(this.premium){
+                return 'free'
+            }
+            return '2.99'
+        },
+    },
+
+})
--- main.js
+++ main.js
@@ -1,50 +1,11 @@
 const app = Vue.createApp({
     data(){
         return{
-            product:'Socks',
-            description:'Lorem ipsum dolor',
-            // image:'./assets/images/socks_green.jpg',
-            selectedVariant:0,
-            url:'google.com',
-            // inStock:true,
-            // inventory:0,
-            onSale:true,
-            details:['50% cotton','30% wool','20% polyester'],
-            variants:[
-                {id:2234,color:'green', image:'./assets/images/socks_green.jpg',quantity:50},
-                {id:2235,color:'blue', image:'./assets/images/socks_blue.jpg',quantity:0}],
             cart:0,
-            brand:'Vue mastery',
+            premium:true,
+            stringaACaso:'voglia di mare',
         }
     },
     methods:{
-     addToCart(){
-         this.cart +=1;
-     },
-     updateVariant(index){
-         this.selectedVariant=index;
-     },
-     downToCart(){
-         if(this.cart>0)
-        this.cart -=1
-        
-     },
-    },
-    // lika calculatore, si aggiorna solo quando serve
-    computed:{
-        title(){
-            return this.brand +' '+this.product
-        },
-        image(){
-            return this.variants[this.selectedVariant].image
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].quantity
-        },
-        titleOnSale(){
-            if(this.onSale)
-            return this.brand +' '+this.product +' is on sale'
-        },
-
     },
 })
