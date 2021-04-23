d1e9118a583fa0e84c8e78ee37958bf67adb3bfb
fazioandrea.f <fazioandrea.f@gmail.com>
2021-04-23 15:57:06 +0200

    end second lesson

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            product:'Socks',
+            description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, laboriosam voluptatum, placeat id deleniti exercitationem culpa nemo non error totam cupiditate laudantium consequatur maiores quasi dolores harum commodi dolore alias.',
+        }
+    }
+})
