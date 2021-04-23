d9ab006960b86924063e7e1f167a148447ce4371
fazioandrea.f <fazioandrea.f@gmail.com>
2021-04-23 16:43:27 +0200

    end ex 6

--- main.js
+++ main.js
@@ -8,6 +8,24 @@ const app = Vue.createApp({
             inStock:true,
             inventory:0,
             onSale:true,
+            details:['50% cotton','30% wool','20% polyester'],
+            variants:[
+                {id:2234,color:'green', image:'./assets/images/socks_green.jpg'},
+                {id:2235,color:'blue', image:'./assets/images/socks_blue.jpg'}],
+            cart:0,
         }
+    },
+    methods:{
+     addToCart(){
+         this.cart +=1;
+     },
+     updateImage(variantImage){
+         this.image=variantImage
+     },
+     downToCart(){
+         if(this.cart>0)
+        this.cart -=1
+        
+     }
     }
 })
