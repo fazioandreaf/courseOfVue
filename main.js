695af3e39133ccf222b4eb3655661c2f318a4540
fazioandrea.f <fazioandrea.f@gmail.com>
2021-04-23 16:16:05 +0200

    finita 3 lezione

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data(){
         return{
             product:'Socks',
-            description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, laboriosam voluptatum, placeat id deleniti exercitationem culpa nemo non error totam cupiditate laudantium consequatur maiores quasi dolores harum commodi dolore alias.',
+            description:'Lorem ipsum dolor',
+            image:'./assets/images/socks_green.jpg',
+            url:'google.com'
         }
     }
 })
